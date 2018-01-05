bf1304eca25c401f5a1fc779e58da1f7d40dc24c
jmkizer <jasonkizer@sonic.net>
2018-01-05 22:41:26 +0000

    Update install-unifi.sh
    Updated for 5.7.12.

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,7 +4,7 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine or FreeBSD Jail running on FreeNAS.
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ubnt.com/unifi/5.7.11-823d14dff6/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ubnt.com/unifi/5.7.12-f5afb57178/UniFi.unix.zip"
 
 # The rc script associated with this branch or fork:
 RC_SCRIPT_URL="https://raw.githubusercontent.com/jmkizer/unifi-controller-freebsd-freenas/5.7.x/rc.d/unifi.sh"
